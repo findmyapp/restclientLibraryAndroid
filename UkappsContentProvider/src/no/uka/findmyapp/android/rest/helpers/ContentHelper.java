b08b2de6117cb8174f0a85057479da2f573d07e7
Jostein Guldal <jostein.guldal@accenture.com>
2011-07-14 16:09:20 +0200

    .

---
+++ UkappsContentProvider/src/no/uka/findmyapp/android/rest/helpers/ContentHelper.java
@@ -0,0 +1,35 @@
+package no.uka.findmyapp.android.rest.helpers;
+
+import java.io.Serializable;
+
+import no.uka.findmyapp.android.rest.mapper.IContentMapper;
+import android.content.ContentValues;
+import android.util.Log;
+
+public class ContentHelper {
+	private static final String MAPPER_POSTFIX = "ContentMapper";
+	private static final String MAPPER_PREFIX = "no.uka.findmyapp.android.rest.mapper.";
+	public static ContentValues getContentValues(Serializable object) {
+		
+		try {
+			
+			String classString = MAPPER_PREFIX + object.getClass().getSimpleName() + MAPPER_POSTFIX;
+			Log.v("DEBUG", classString);
+			Log.v("DEBUG", object.getClass().getSimpleName());
+			Log.v("DEBUG", object.getClass().getCanonicalName());
+			Class<IContentMapper> mapperClass = (Class<IContentMapper>)  Class.forName(classString);
+			IContentMapper contentMapper = (IContentMapper) mapperClass.newInstance();
+			return contentMapper.mapValues(object);
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+}

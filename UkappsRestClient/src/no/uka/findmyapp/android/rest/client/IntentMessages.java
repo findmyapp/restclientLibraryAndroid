b92ac7ad41bd86a7d8e631022b913c249e7f275f
Torstein Melhus Barkve <torstein@barkve.no>
2011-07-17 17:38:51 +0200

    - Removed dublicated code in RestProcessor - Cleaned old comments - Fixed Git automerge screwups - Renamed BroadcastTokens class to IntentMessages - Added typesafe constant to identify the extrabundle carrying the   returndata from the broadcast intent sent from RestProcessor - Added typesafe extradata identifiers in IntentMessages, SERVICE_MODEL_PACKAGE - Renamed startServiceTest to callStartService in RestServiceHelper - Updated connection settings in UkappsServiceFactory

---
+++ UkappsRestClient/src/no/uka/findmyapp/android/rest/client/IntentMessages.java
@@ -0,0 +1,8 @@
+package no.uka.findmyapp.android.rest.client;
+
+public final class IntentMessages {
+	public static final String SERVICE_MODEL_PACKAGE = "ServiceModel";
+	
+	public static final String BROADCAST_INTENT_TOKEN = "no.uka.findmyapp.android.demo.BROADCAST_INTENT_TOKEN";
+	public static final String BROADCAST_RETURN_VALUE_NAME = "serialized.data.return.package";
+}
